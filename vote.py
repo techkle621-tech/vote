324f22f94d88292dbaaf05ec726551d14cdd5c42
techkle621-tech <techkle621@gmail.com>
2026-01-19 13:10:22 +0530

    Update vote.py

--- vote.py
+++ vote.py
@@ -1,6 +1,8 @@
 try:
-  age=int(input("Enter your age"))
-  if age>=18
-  print("you are eligible to vote")
-  else
-  print("you are not eligible to vote")
+    age = int(input("Enter your age: "))
+    if age >= 18:
+        print("You are eligible to vote")
+    else:
+        print("You are not eligible to vote")
+except ValueError:
+    print("Invalid input! Please enter a valid number.")
